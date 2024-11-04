4a1b39d592a1b6b5f5fcc5610229130eecc142a5
marcin-jamroz <17850284+marcin-jamroz@users.noreply.github.com>
2024-11-04 18:46:15 +0100

    bluetooth fix cd

--- system/configuration.nix
+++ system/configuration.nix
@@ -107,8 +107,8 @@
     POLKIT_BIN = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
   };
 
-  hardware.enableRedistributableFirmware = true;
-
+  # hardware.enableRedistributableFirmware = true;
+  # hardware.enableAllFirmware = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
