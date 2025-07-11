9cd609b77c6f84c665bf23c88dcf047ddfbfd645
marcin-jamroz <17850284+marcin-jamroz@users.noreply.github.com>
2025-07-11 19:02:53 +0200

    upgrade packages

--- system/configuration.nix
+++ system/configuration.nix
@@ -60,7 +60,7 @@
 
   # Enable the GNOME Desktop Environment.
   # services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = false;
+  services.desktopManager.gnome.enable = false;
 
   # Configure keymap in X11
   services.xserver.xkb = {
