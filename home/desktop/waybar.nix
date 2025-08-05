ebddb1ad44fff9184e2c40dc87dd000eccd1f665
marcin-jamroz <17850284+marcin-jamroz@users.noreply.github.com>
2025-08-05 20:04:36 +0200

    upgrade packages

--- home/desktop/waybar.nix
+++ home/desktop/waybar.nix
@@ -115,7 +115,7 @@ with lib;
         "hyprland/language" = {
           format-en = "us";
           format-pl = "pl";
-          on-click = "hyprctl switchxkblayout current next";
+          on-click = "sleep 0.1 && hyprctl switchxkblayout current next";
         };
         "memory" = {
           interval = 10;
