2c5cb83a836118e188c0f4c0732d1b8631b8d079
marcin-jamroz <17850284+marcin-jamroz@users.noreply.github.com>
2025-10-19 19:33:42 +0200

    fix sddm

--- system/modules/display-manager.nix
+++ system/modules/display-manager.nix
@@ -11,18 +11,14 @@
     enable = true;
     autoNumlock = true;
     wayland.enable = true;
-    theme = "tokyo-night-sddm";
-    # theme = "sugar-dark";
+    package = pkgs.kdePackages.sddm;
+    theme = "sddm-astronaut-theme";
+    extraPackages = [ pkgs.sddm-astronaut ];
   };
 
-  environment.systemPackages =
-    let
-      sugar = pkgs.callPackage ../custom/display-manager/sddm-sugar-dark.nix { };
-      tokyo-night = pkgs.libsForQt5.callPackage ../custom/display-manager/sddm-tokyo-night.nix { };
-    in
-    [
-      sugar.sddm-sugar-dark # Name: sugar-dark
-      tokyo-night # Name: tokyo-night-sddm
-      pkgs.libsForQt5.qt5.qtgraphicaleffects
-    ];
+  environment.systemPackages = [
+    (pkgs.sddm-astronaut.override {
+      embeddedTheme = "cyberpunk";
+    })
+  ];
 }
