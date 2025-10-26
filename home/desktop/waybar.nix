16ec317c97c0d1c7dda91fcd2609f2064fd23edd
marcin-jamroz <17850284+marcin-jamroz@users.noreply.github.com>
2025-10-26 14:57:18 +0100

    adjust waybar theming

--- home/desktop/waybar.nix
+++ home/desktop/waybar.nix
@@ -230,18 +230,17 @@ with lib;
     ];
     style = concatStrings [
       ''
-              * {
+        * {
         	font-size: 16px;
-        	font-family: Font Awesome, sans-serif;
-            	font-weight: bold;
-              }
-              window#waybar {
+          font-weight: bold;
+        }
+        window#waybar {
         	background-color: #${palette.base00};
         	border-bottom: 1px solid rgba(26,27,38,0);
         	border-radius: 0px;
         	color: #${palette.base0F};
-              }
-              #workspaces {
+        }
+        #workspaces {
         	background: #${palette.base01};
         	margin: 2px;
         	padding: 0px 1px;
@@ -249,42 +248,42 @@ with lib;
         	border: 0px;
         	font-style: normal;
         	color: #${palette.base00};
-              }
-              #workspaces button {
-        	  padding: 0px 5px;
-        	  margin: 4px 3px;
-        	  border-radius: 10px;
-        	  border: 0px;
-        	  color: #${palette.base00};
-                  background: linear-gradient(45deg, #${palette.base0E}, #${palette.base0F}, #${palette.base0D}, #${palette.base09});
-                  background-size: 300% 300%;
-                  animation: gradient_horizontal 15s ease infinite;
-        	  opacity: 0.5;
-                  transition: ${betterTransition};
-              }
-              #workspaces button.active {
-        	  padding: 0px 5px;
-        	  margin: 4px 3px;
-        	  border-radius: 10px;
-        	  border: 0px;
-        	  color: #${palette.base00};
-                  background: linear-gradient(45deg, #${palette.base0E}, #${palette.base0F}, #${palette.base0D}, #${palette.base09});
-                  background-size: 300% 300%;
-                  animation: gradient_horizontal 15s ease infinite;
-                  transition: ${betterTransition};
-        	  opacity: 1.0;
-        	  min-width: 40px;
-              }
-              #workspaces button:hover {
-        	  border-radius: 10px;
-        	  color: #${palette.base00};
-                  background: linear-gradient(45deg, #${palette.base0E}, #${palette.base0F}, #${palette.base0D}, #${palette.base09});
-                  background-size: 300% 300%;
-                  animation: gradient_horizontal 15s ease infinite;
-        	  opacity: 0.8;
-                  transition: ${betterTransition};
-              }
-              @keyframes gradient_horizontal {
+        }
+        #workspaces button {
+        	padding: 0px 5px;
+        	margin: 4px 3px;
+        	border-radius: 10px;
+        	border: 0px;
+          color: #${palette.base00};
+          background: linear-gradient(45deg, #${palette.base0E}, #${palette.base0F}, #${palette.base0D}, #${palette.base09});
+          background-size: 300% 300%;
+          animation: gradient_horizontal 15s ease infinite;
+          opacity: 0.5;
+          transition: ${betterTransition};
+        }        
+        #workspaces button.active {
+        	padding: 0px 5px;
+        	margin: 4px 3px;
+        	border-radius: 10px;
+        	border: 0px;
+        	color: #${palette.base00};
+          background: linear-gradient(45deg, #${palette.base0E}, #${palette.base0F}, #${palette.base0D}, #${palette.base09});
+          background-size: 300% 300%;
+          animation: gradient_horizontal 15s ease infinite;
+          transition: ${betterTransition};
+        	opacity: 1.0;
+        	min-width: 40px;
+        }
+        #workspaces button:hover {
+        	border-radius: 10px;
+        	color: #${palette.base00};
+          background: linear-gradient(45deg, #${palette.base0E}, #${palette.base0F}, #${palette.base0D}, #${palette.base09});
+          background-size: 300% 300%;
+          animation: gradient_horizontal 15s ease infinite;
+        	opacity: 0.8;
+          transition: ${betterTransition};
+        }
+        @keyframes gradient_horizontal {
         	0% {
         	  background-position: 0% 50%;
         	}
@@ -294,15 +293,15 @@ with lib;
         	100% {
         	  background-position: 0% 50%;
         	}
-              }
-              @keyframes swiping {
-                0% {
+        }
+        @keyframes swiping {
+          0% {
         	  background-position: 0% 200%;
         	}
         	100% {
         	  background-position: 200% 200%;
         	}
-              }
+        }
               tooltip {
         	background: #${palette.base00};
         	border: 1px solid #${palette.base0E};
